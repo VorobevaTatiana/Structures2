7b9bfe278588eee78ab8ef80256464e2426ec514
Tatiana V <IgVo@rambler.ru>
2022-11-26 14:33:14 +0300

    Initial commit

---
+++ Structures2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+struct bank {
+    int account;
+    std::string name;
+    double sum;
+};
+void change_account(bank& bk, int& new_sum)
+{
+    bk.sum = new_sum;
+    std::cout << "Ваш счет:" << bk.name << ", " << bk.account << ", " << new_sum << std::endl;
+}
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    system("chcp 1251");
+
+    int new_sum;
+
+    struct bank arg;
+    std::cout << "Введите номер счета:";
+    std::cin >> arg.account;
+    std::cout << "Введите имя:";
+    std::cin >> arg.name;
+    std::cout << "Введите баланс счета:";
+    std::cin >> arg.sum;
+    std::cout << "Введите новый баланс счета:";
+    std::cin >> new_sum;
+
+    change_account(arg, new_sum);
+              
+}
